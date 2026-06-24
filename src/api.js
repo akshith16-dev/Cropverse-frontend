9158570f75e45940b125f626e2875a5765c2bef5
AJoshna02 <anangijoshna@gmail.com>
2026-06-24 15:15:19 +0530

    arise

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL:
+    import.meta.env.VITE_API_URL || "https://cropverse-backend.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
